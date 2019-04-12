ddd2979956022879b1255e2cbeb9f34bfbb32d17
obelouch <34724655+XD-OB@users.noreply.github.com>
2019-04-12 06:38:09 +0100

    Update brackets.c

--- Corrections/brackets/brackets.c
+++ Corrections/brackets/brackets.c
@@ -3,41 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   brackets.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ishaimou <ishaimou@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: obelouch <obelouch@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/29 05:11:37 by ishaimou          #+#    #+#             */
-/*   Updated: 2019/01/29 05:12:18 by ishaimou         ###   ########.fr       */
+/*   Created: 2019/03/01 04:01:27 by obelouch          #+#    #+#             */
+/*   Updated: 2019/03/29 05:13:34 by obelouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	braclose(char *str, char c, int i, int b)
+int			closni(char *str, char c)
 {
-	while (b && *(++str) && (i++))
-		if (*str == c || *str == c + c % 2 + 1)
-			*str == c ? ++b : --b;
-	return (i);
+	int		loop = 1;
+	int		distance = 1;
+
+	while (loop && *str)
+	{
+		if (*str == c)
+			loop++;
+		if (*str == c + c % 2 + 1)
+			loop--;
+		distance++;
+		str++;
+	}
+	return (distance);
 }
 
-int	brackets(char *str, char c)
+int			brackets(char *str, char c)
 {
 	if (*str == c)
 		return (1);
 	else if (!*str || *str == ')' || *str == '}' || *str == ']')
 		return (0);
-	else if (*str == '(' || *str == '{' || *str == '[')
-		return (brackets(str + 1, *str + *str % 2 + 1)
-			* brackets(str + braclose(str, *str, 1, 1), c));
+	else if (*str == '{' || *str == '(' || *str == "[")
+		return (
+				brackets(str + 1, *str + *str % 2 + 1)
+				* brackets(str + closni(str + 1, *str), c)
+				);
 	else
 		return (brackets(str + 1, c));
 }
 
-int	main(int ac, char **av)
+int			main(int ac, char **av)
 {
-	int	i;
+	int		i = 0;
 
-	i = 0;
 	if (ac > 1)
 		while (++i < ac)
 			brackets(av[i], 0) ? write(1, "OK\n", 3) : write(1, "Error\n", 6);
